53328df48a724a3dfed8484bb26bf6a2a529a7ed
enrico.hu <enrico.hu@PC2214BIS>
2025-04-30 12:53:37 +0200

    creazione diagramma di classi

---
+++ MauiApp1/MauiApp1/Mine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MauiApp1
+{
+    public class Mine
+    {
+        public int ListaMine
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void RandomMine(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MauiApp1/MauiApp1/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MauiApp1
+{
+    public class ViewModel
+    {
+        Mine mine;
+        Model model;
+        public bool trovataLaMina;
+
+        public Mine Mine
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Model Model
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        //con il Tentativo verifico se ho cliccato la mina
+        public void GestioneDifficoltà()
+        {
+            throw new System.NotImplementedException();
+            
+        }
+
+        public void GrandezzaPersonalizzata(int x, int y)
+        {
+            //x e y sono la lunghezza e larghezza del campo
+        }
+
+        public void Tentativo(int x, int y)
+        {
+            throw new System.NotImplementedException();
+            //posizione che l'utente clicca nella matrice
+           
+        }
+        public bool Vittoria()
+        {
+            if(trovataLaMina == true)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
